1fda72eafafca2990d52a35202f708f8a22fee48
Cristian Agudelo <cristianmira39hotmail.com>
2024-06-11 19:10:55 -0500

    Se implementa la configuración en el archivo nuxt.config.ts #3

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-  // Aqui se incluye la configuración necesaria
+  devtools: { enabled: true },
+  head: {
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+  },
+  devtools: { enabled: true },
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify", "vuejs3-datepicker"],
+  },
+  generate: {
+    dir: "dist",
+  },
+  pinia: {
+    stores: [{ name: "auth", path: "~/store/auth.js" }],
+  },
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+  },
 });
