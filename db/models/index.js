afed8c7e8ebe05b2a648351dc69a70248fcfc3e1
AndresRojo12 <andresrojo20013@gmail.com>
2024-06-27 17:02:25 -0500

    se inicializa la asociación del modelo product #11

--- db/models/index.js
+++ db/models/index.js
@@ -2,12 +2,17 @@ const sequelize = require('../../libs/sequelize');
 
 const { User, UserSchema } = require('./user.model');
 const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema} = require('./product.model');
+
 
 const setupModels = (sequelize) => {
   User.init(UserSchema, User.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 
-  Category.associate(sequelize.models)
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
+
 };
 
 module.exports = setupModels;
