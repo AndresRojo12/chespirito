6768af19203f3528aab364340f2a7d5e9a5e269a
AndresRojo12 <andresrojo20013@gmail.com>
2024-10-22 14:21:46 -0500

    jj

--- db/config.js
+++ db/config.js
@@ -4,8 +4,7 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = process.env.DATABASE_URL || `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
