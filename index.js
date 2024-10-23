5a69a20964de888b664accdf5862fe18564b5588
AndresRojo12 <andresrojo20013@gmail.com>
2024-10-22 19:12:35 -0500

    jj

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ const optionCors = {
 
 app.use(cors(optionCors));
 
-// // Servir archivos estáticos de la aplicación frontend (Nuxt.js)
-// const nuxtDistPath = path.join(__dirname, './frontend/dist');
-// app.use(express.static(nuxtDistPath));
+// Servir archivos estáticos de la aplicación frontend (Nuxt.js)
+const nuxtDistPath = path.join(__dirname, './frontend/dist');
+app.use(express.static(nuxtDistPath));
 
-// // Ruta para servir el archivo HTML principal de la aplicación frontend
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(nuxtDistPath, 'index.html'));
-// });
+// Ruta para servir el archivo HTML principal de la aplicación frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(nuxtDistPath, 'index.html'));
+});
 
 
 app.get('/',(req, res) => {
