e667a4f45db16e2f5415850f920b0623e706288d
AndresRojo12 <andresrojo20013@gmail.com>
2024-10-23 14:03:54 -0500

    jjnjhj

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   head: {
     meta: [
@@ -23,8 +22,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      ApiBaseUrl: process.env.API_BASE_URL,
+      API_BASE_URL: process.env.API_BASE_URL,
     },
   },
-
 });
