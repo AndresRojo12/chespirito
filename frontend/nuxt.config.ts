91c5fbf2f56536b1fb1bd2f654501b4d5e48e044
AndresRojo12 <andresrojo20013@gmail.com>
2024-11-02 12:06:15 -0500

    coreccion ruta

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,25 +4,33 @@ export default defineNuxtConfig({
       { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
   },
+
   ssr: true,
   target: 'static',
   devtools: { enabled: true },
+
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.min.css",
   ],
+
   build: {
     transpile: ["vuetify", "vuejs3-datepicker"],
   },
+
   generate: {
     dir: "dist",
   },
+
   pinia: {
     stores: [{ name: "auth", path: "~/store/auth.js" }],
   },
+
   runtimeConfig: {
     public: {
       API_BASE_URL: process.env.API_BASE_URL,
     },
   },
+
+  compatibilityDate: "2024-11-02",
 });
