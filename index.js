be50ce80c0481e2c21c7b0f0d863283b91eaba6e
AndresRojo12 <andresrojo20013@gmail.com>
2024-11-02 11:00:05 -0500

    coreccion ruta

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const routerApi = require('./routes');
 require('./utils/auth');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
