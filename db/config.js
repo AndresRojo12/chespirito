c5d209ee2b1b70df58f71db46296ed05b899649a
AndresRojo12 <andresrojo20013@gmail.com>
2024-11-09 20:23:09 -0500

    se hace configuracion sequelize

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}?sslmode=require`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
